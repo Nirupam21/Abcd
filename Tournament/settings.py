5db2b7ad9f2240942f0516be5833499b79fed950
Nirupam21 <36543804+Nirupam21@users.noreply.github.com>
2018-10-29 23:24:05 +0530

    Update settings.py

--- Tournament/settings.py
+++ Tournament/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
